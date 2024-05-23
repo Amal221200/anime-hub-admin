720fe56f528f275f3906089094c059100d5c67d7
Amal221200 <amalmurikkoli2000@gmail.com>
2024-05-23 17:11:55 +0530

    loading

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,14 +1,17 @@
-import React, { Suspense } from 'react'
-import DesktopSidebar from './_components/desktop-sidebar'
+import React, { lazy, Suspense } from 'react'
 import MobileNavbar from './_components/MobileNavbar'
 import MobileUserButton from './_components/MobileUserButton'
 import SkeletonSpinner from '@/components/SkeletonSpinner'
+
+const DesktopSidebar = lazy(() => import('./_components/desktop-sidebar'))
 
 function AdminLayout({ children }: { children: React.ReactNode }) {
     return (
         <div>
             <MobileUserButton />
-            <Suspense fallback={<SkeletonSpinner />}>
+            <Suspense fallback={
+                <SkeletonSpinner className='fixed left-2 top-10 hidden h-[89vh] w-[200px] items-center justify-center gap-y-4 rounded-md border bg-zinc-900/20 p-2 sm:flex md:left-7' />
+            }>
                 <DesktopSidebar />
             </Suspense>
             <div className='min-h-screen px-3 pt-10 sm:ml-[220px] sm:px-4 md:ml-[250px]'>
