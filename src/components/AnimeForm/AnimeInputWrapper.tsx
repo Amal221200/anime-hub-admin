534e246341e9c3afae9e8c3a47a51f9d43533815
Amal221200 <amalmurikkoli2000@gmail.com>
2024-05-28 11:12:34 +0530

    refactor

---
+++ src/components/AnimeForm/AnimeInputWrapper.tsx
@@ -0,0 +1,36 @@
+import React, { ReactNode } from 'react'
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
+import { ControllerRenderProps, UseFormReturn } from 'react-hook-form'
+import { z } from 'zod'
+import { animeFormSchema } from '@/lib/schema'
+import { cn } from '@/lib/utils'
+
+type inputTypes = keyof z.infer<typeof animeFormSchema>
+
+function AnimeInputWrapper<T extends inputTypes>({ form, name, children, label, className }: {
+    form: UseFormReturn<z.infer<typeof animeFormSchema>>,
+    name: T,
+    children: ((field: ControllerRenderProps<z.infer<typeof animeFormSchema>, T>) => ReactNode),
+    label: string,
+    className?: string
+}) {
+    return (
+        <FormField
+            control={form.control}
+            name={name}
+            render={({ field }: { field: ControllerRenderProps<z.infer<typeof animeFormSchema>, T> }) => (
+                <FormItem className={cn('', className)}>
+                    <FormLabel>
+                        {label}
+                    </FormLabel>
+                    <FormControl>
+                        {children(field)}
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )}
+        />
+    )
+}
+
+export default AnimeInputWrapper
--- src/components/AnimeForm/index.tsx
+++ src/components/AnimeForm/index.tsx
@@ -6,11 +6,7 @@ import { Button } from "@/components/ui/button"
 import { animeFormSchema, animeSchema } from "@/lib/schema"
 import {
     Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+    FormControl
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select"
@@ -20,8 +16,9 @@ import useCurrentUser from "@/hooks/current-user/useCurrentUser"
 import { addAnime, editAnime } from "../functions"
 import { ANIME_FORM_TYPE } from "@/lib/types"
 import useAlertModal from "@/hooks/useAlertModal"
-import DateInput from "./DateInput"
+import DateInput from "../DateInput"
 import useAnimeForm from "@/hooks/anime/useSubmitAnimeForm"
+import AnimeInputWrapper from "./AnimeInputWrapper"
 
 interface AnimeFormProps {
     anime?: Anime,
@@ -33,10 +30,10 @@ const AnimeForm = ({ anime, heading, type }: AnimeFormProps) => {
     const { onOpen } = useAlertModal()
     const { data: userData } = useCurrentUser()
     const { form, mutateAsync } = useAnimeForm(type, type === ANIME_FORM_TYPE.ADD ? addAnime : editAnime(anime?.id as string), anime)
-    
+
     const onSubmit = useCallback(async (values: z.infer<typeof animeFormSchema>) => {
         const payload = animeSchema.parse({ ...values, genre: values.genre.split(','), episodes: parseInt(values.episodes), episodeDuration: parseInt(values.episodeDuration) })
-
+        
         await mutateAsync({ data: payload })
     }, [mutateAsync])
 
@@ -51,193 +48,97 @@ const AnimeForm = ({ anime, heading, type }: AnimeFormProps) => {
             <form onSubmit={form.handleSubmit(onSubmit)} className="mx-auto mb-16 max-w-[380px] space-y-3 sm:mb-0 sm:max-w-[1000px]">
                 <h1 className="text-3xl font-semibold sm:text-5xl">{heading}</h1>
                 <div className="mb-3 grid grid-cols-1 gap-2 sm:grid-cols-2">
-                    <FormField
-                        control={form.control}
-                        name="title"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>
-                                    Title
-                                </FormLabel>
-                                <FormControl>
-                                    <Input {...field} placeholder="eg: Dragon Ball Z" />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
+                    <AnimeInputWrapper name="title" form={form} label="Title">
+                        {(field) => (
+                            <Input {...field} placeholder="eg: Dragon Ball Z" />
                         )}
-                    />
+                    </AnimeInputWrapper>
 
-                    <FormField
-                        control={form.control}
-                        name="artist"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>
-                                    Artist
-                                </FormLabel>
-                                <FormControl>
-                                    <Input {...field} placeholder="eg: Akira Toriyama" />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
+                    <AnimeInputWrapper name="artist" form={form} label="Artist">
+                        {(field) => (
+                            <Input {...field} placeholder="eg: Akira Toriyama" />
                         )}
-                    />
+                    </AnimeInputWrapper>
 
-                    <FormField
-                        control={form.control}
-                        name="genre"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>
-                                    Genre
-                                </FormLabel>
-                                <FormControl>
-                                    <Input {...field} placeholder="eg: Shonen, Action" />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
+                    <AnimeInputWrapper name="genre" form={form} label="Genre">
+                        {(field) => (
+                            <Input {...field} placeholder="eg: shonen, action, adventure" />
                         )}
-                    />
+                    </AnimeInputWrapper>
+                    <AnimeInputWrapper name="studio" form={form} label="Studio">
+                        {(field) => (
+                            <Input {...field} placeholder="eg: Toe Animation" />
+                        )}
+                    </AnimeInputWrapper>
 
-                    <FormField
-                        control={form.control}
-                        name="studio"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>
-                                    Studio
-                                </FormLabel>
+                    <AnimeInputWrapper name="status" form={form} label="Status">
+                        {(field) => (
+                            <Select onValueChange={field.onChange} defaultValue={field.value}>
                                 <FormControl>
-                                    <Input {...field} placeholder="eg: Toe Animation" />
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="eg: COMPLETED" />
+                                    </SelectTrigger>
                                 </FormControl>
-                                <FormMessage />
-                            </FormItem>
+                                <SelectContent>
+                                    {['ONGOING', 'COMPLETED'].map((status) => (
+                                        <SelectItem key={status} value={status} >
+                                            {status}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
                         )}
-                    />
+                    </AnimeInputWrapper>
 
-                    <FormField
-                        control={form.control}
-                        name="status"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>
-                                    Status
-                                </FormLabel>
-                                <FormControl>
-                                    <Select onValueChange={field.onChange} defaultValue={field.value}>
-                                        <FormControl>
-                                            <SelectTrigger>
-                                                <SelectValue placeholder="eg: COMPLETED" />
-                                            </SelectTrigger>
-                                        </FormControl>
-                                        <SelectContent>
-                                            {['ONGOING', 'COMPLETED'].map((status) => (
-                                                <SelectItem key={status} value={status} >
-                                                    {status}
-                                                </SelectItem>
-                                            ))}
-                                        </SelectContent>
-                                    </Select>
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
 
-                    <FormField
-                        control={form.control}
-                        name="watchLink"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>
-                                    WatchLink
-                                </FormLabel>
-                                <FormControl>
-                                    <Input {...field} placeholder="eg: https://example.com" />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
+                    <AnimeInputWrapper name="watchLink" label="Watch Link" form={form}>
+                        {
+                            (field) => (
+                                <Input {...field} placeholder="eg: https://example.com" />
+                            )
+                        }
+                    </AnimeInputWrapper>
 
-                    <FormField
-                        control={form.control}
-                        name="episodes"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>
-                                    Episodes
-                                </FormLabel>
-                                <FormControl>
-                                    <Input {...field} placeholder="eg: 131" />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
+                    <AnimeInputWrapper name="episodes" label="Episodes" form={form}>
+                        {
+                            (field) => (
+                                <Input {...field} placeholder="eg: 131" />
+                            )
+                        }
+                    </AnimeInputWrapper>
 
-                    <FormField
-                        control={form.control}
-                        name="episodeDuration"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>
-                                    Episode Duration
-                                </FormLabel>
-                                <FormControl>
-                                    <Input {...field} placeholder="eg: 23 mins" />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
+                    <AnimeInputWrapper name="episodeDuration" label="Episode Duration" form={form}>
+                        {
+                            (field) => (
+                                <Input {...field} placeholder="eg: 23 mins" />
+                            )
+                        }
+                    </AnimeInputWrapper>
 
-                    <FormField
-                        control={form.control}
-                        name="release"
-                        render={({ field }) => (
-                            <FormItem className="flex flex-col">
-                                <FormLabel>
-                                    Release
-                                </FormLabel>
-                                <FormControl>
-                                    <DateInput field={field} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="imageLink"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>
-                                    Image Link
-                                </FormLabel>
-                                <FormControl >
-                                    <FileUpload endpoint="animeImage" value={field.value} onChange={field.onChange} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="description"
-
-                        render={({ field }) => (
-                            <FormItem className="sm:col-span-2">
-                                <FormLabel>
-                                    Description
-                                </FormLabel>
-                                <FormControl>
-                                    <Textarea {...field} placeholder="eg: This is the legend of a young kid called Son Goku..." rows={6} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
+                    <AnimeInputWrapper name="release" label="Release" form={form} className="flex flex-col">
+                        {
+                            (field) => (
+                                <DateInput field={field} />
+                            )
+                        }
+                    </AnimeInputWrapper>
+                    
+                    <AnimeInputWrapper name="imageLink" label="Image Link" form={form}>
+                        {
+                            (field) => (
+                                <FileUpload endpoint="animeImage" value={field.value} onChange={field.onChange} />
+                            )
+                        }
+                    </AnimeInputWrapper>
+                    
+                    <AnimeInputWrapper name="description" label="Description" form={form} className="col-span-2">
+                        {
+                            (field) => (
+                                <Textarea {...field} placeholder="eg: This is the legend of a young kid called Son Goku..." rows={6} 
+                                className="no-scrollbar"/>
+                            )
+                        }
+                    </AnimeInputWrapper>
                 </div>
                 <Button type="submit"
                     disabled={form.formState.isLoading || form.formState.isSubmitting || userData?.role === 'USER'} className="block w-full disabled:cursor-null disabled:opacity-60 sm:w-max">
