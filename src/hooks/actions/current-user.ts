682cdc343634f4b21b7578c944c5e010a1a62b1e
Amal221200 <amalmurikkoli2000@gmail.com>
2024-06-01 17:45:32 +0530

    cleanup

--- src/hooks/actions/current-user.ts
+++ src/hooks/actions/current-user.ts
@@ -2,7 +2,6 @@ import { AdminUser } from "@prisma/client";
 import axios from "axios";
 
 export async function fetchCurrentUser(): Promise<AdminUser> {
-    const response = await axios.get('/api/admin-user/currentUser');
-
+    const response = await axios.get('/api/current-user');
     return response.data
 }
