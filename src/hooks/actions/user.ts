9df71e59bb5c466a6550bb2d9bfb4108251d32f4
Amal221200 <amalmurikkoli2000@gmail.com>
2024-06-01 13:36:49 +0530

    clean-up

--- src/hooks/actions/user.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "@prisma/client";
-import axios from "axios";
-
-export async function fetchUsers(): Promise<User[]> {
-    const users = await axios.get('/api/user');
-    return users.data;
-}
