9631177acc8023c53713efa48c84092022aab489
Amal221200 <amalmurikkoli2000@gmail.com>
2024-06-09 11:42:09 +0530

    image compression

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -5,6 +5,7 @@ import { UploadDropzone } from "@/lib/uploadthing"
 import { cn } from "@/lib/utils"
 import useCurrentUser from "@/hooks/current-user/useCurrentUser"
 import { toast } from "sonner"
+import imageCompression, { Options } from "browser-image-compression"
 
 interface FileUploadProps {
     onChange: (url?: string, name?: string) => void,
@@ -13,11 +14,29 @@ interface FileUploadProps {
     preview?: boolean
 }
 
+async function compress(file: File) {
+    const imageFile = file;
+
+    const options: Options = {
+        maxSizeMB: 0.6,
+        maxWidthOrHeight: 1920,
+        useWebWorker: true,
+    }
+    try {
+        const compressedFile = await imageCompression(imageFile, options);
+        return compressedFile
+    } catch (error) {
+        console.log(error);
+    }
+
+    return file;
+}
+
 const FileUpload: React.FC<FileUploadProps> = ({ endpoint, onChange, value, preview }) => {
     const { data: userData } = useCurrentUser()
     const fileType = value.split('.').at(-1)
 
-    if (value && fileType !== 'pdf' && preview) {
+    if (value && (fileType !== 'pdf') && preview) {
         return (
             <div className="relative h-20 w-20">
                 <Image src={value} alt="server-image" fill className="rounded-full object-cover" />
@@ -29,15 +48,29 @@ const FileUpload: React.FC<FileUploadProps> = ({ endpoint, onChange, value, prev
     }
     return (
         <UploadDropzone endpoint={endpoint} className={cn("upload-button  cursor-pointer", userData?.role === 'USER' && 'cursor-null  pointer-events-none opacity-60')} appearance={{ uploadIcon: { display: 'none' }, label: { display: 'none' }, allowedContent: { display: 'none' }, container: { padding: 0, margin: 0, display: "block" }, button: { padding: 0, marginInline: 0, marginBlock: 10 } }}
+
+            onDrop={(files) => {
+                if (files[0].size / 1024 / 1024 > 4) {
+                    return toast.error('File is too big, max file size is 4MB')
+                }
+                return []
+            }}
             onClientUploadComplete={(res) => {
                 onChange(res?.[0].url, res?.[0].name)
                 toast.success("Upload complete", { description: `${res?.[0].name} was uploaded` });
-            }} onUploadError={(error) => {
+            }}
+
+            onUploadError={(error) => {
                 if (error.message.includes('FileSizeMismatch')) {
                     toast.error("Upload Error", { description: "File is too big, the maximum file size is 4MB" });
                 }
                 console.log(error);
-            }} />
+            }}
+
+            onBeforeUploadBegin={(files) => {
+                return compress(files[0]).then(file => [file])
+            }}
+        />
     )
 }
 
--- src/components/blog-editor/bubble-menu/BubbleMenuImage.tsx
+++ src/components/blog-editor/bubble-menu/BubbleMenuImage.tsx
@@ -1,60 +1,21 @@
 "use client"
-import { Ruler, Trash2 } from "lucide-react";
+import { Trash2 } from "lucide-react";
 import { Toggle } from "../../ui/toggle";
-import { useCallback, useRef } from "react";
 import { Editor } from "@tiptap/react";
 import { BubbleMenu } from "@tiptap/react";
-import AddImage from "../AddImage";
 import { bubbleMenuData } from "@/lib/constants";
 
 const BubbleMenuImage = ({ editor }: { editor: Editor | null }) => {
-  const handleUpload = useCallback((url: string, name: string) => {
-    editor?.chain().focus().setImage({ src: url, alt: `${name} ${crypto.randomUUID()}` }).run()
-  }, [editor])
-
-
-  const applyClass = useCallback(() => {
-    if (!editor) {
-      return
-    }
-    const { state, dispatch } = editor.view
-    const { tr, selection } = state
-    const { $from, $to } = selection
-    let updated = false
-
-    state.doc.nodesBetween($from.pos, $to.pos, (node, pos) => {
-      if (node.type.name === 'resizable-image') {
-        const newAttrs = {
-          width: editor.extensionStorage['resizable-image'].width,
-          src: node.attrs.src,
-          alt: node.attrs.alt,
-          class: node.attrs.class.includes('fix-dimension') ? node.attrs.class.replace(' fix-dimension', '') : `${node.attrs.class} fix-dimension`,
-        }
-
-        tr.setNodeMarkup(pos, undefined, newAttrs)
-        updated = true
-      }
-    })
-
-    if (updated) {
-      dispatch(tr)
-      editor.commands.focus()
-    }
-  }, [editor])
-
+  
   if (!editor) {
     return null
   }
 
   return (
-    <BubbleMenu editor={editor} className={bubbleMenuData.image.class} shouldShow={({ editor }) => editor?.isActive("resizable-image")}
+    <BubbleMenu editor={editor} className={bubbleMenuData.image.class} shouldShow={({ editor }) => editor?.isActive("image")}
       pluginKey={bubbleMenuData.image.pluginKey}>
       <div className="flex w-max items-center gap-x-2 rounded-md border border-input bg-background/40 backdrop-blur-sm sm:gap-x-4">
         <div className="flex items-center gap-x-2 rounded-md">
-          {/* <AddImage onUploadComplete={handleUpload} bubble focused={editor?.isActive("resizable-image")} />
-          <Toggle size="xs" title="fix-dimension" pressed={editor.$node('resizable-image')?.attributes?.class?.includes('fix-dimension')} onPressedChange={applyClass}>
-            <Ruler size={15} />
-          </Toggle> */}
           <Toggle size="xs" title="delete-image" pressed={!editor.can().deleteSelection()} className="hover:text-red-400/90" onPressedChange={() => {
             editor.chain().focus().deleteSelection().run()
 
